7f1f74f8371d0a4f59f0620a9823aedf82094121
Aaron Scherer <aequasi@gmail.com>
2013-04-30 15:42:29 -0700

    Fixing configs again

---
+++ Command/SetCommand.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author    Aaron Scherer <aequasi@gmail.com>
+ * @date 2013
+ * @license   http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
+ */
+namespace Aequasi\Bundle\MemcachedBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * GetCommand
+ *
+ * Grabs the given key out of cache
+ */
+class SetCommand extends ContainerAwareCommand
+{
+
+	protected function configure()
+	{
+		$this->setName( 'memcached:get' )
+			->setDescription( 'Set a key\'s value to memcached' )
+			->addArgument( 'key', InputArgument::REQUIRED, 'What key do you want to set' )
+			->addArgument( 'value', InputArgument::REQUIRED, 'What do you want the value to be' );
+	}
+
+	protected function execute( InputInterface $input, OutputInterface $output )
+	{
+		$key = $input->getArgument( 'key' );
+		$value = $input->getArgument( 'value' );
+
+		$this->getContainer()->get( 'memcached' )->set( $key, $value );
+
+		$output->writeln( sprintf( '<info>Key: %s', $key ) );
+		$output->writeln( sprintf( '<info>Value: %s', $value ) );
+		$output->writeln( "\n" );
+	}
+}
