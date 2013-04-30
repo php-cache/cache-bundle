26cd78b7e310aeb01c426ebebcb6f993fdc0bcc4
Aaron Scherer <aequasi@gmail.com>
2013-04-30 15:27:11 -0700

    Fixing configs again

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -82,7 +82,6 @@ class Configuration implements ConfigurationInterface
 		$node = $treeBuilder->root( 'options' );
 
 		$node
-			->addDefaultsIfNotSet()
 			->children()
 				->booleanNode( 'compression' )
 					->info( 'Enables or disables payload compression. When enabled, item values longer than a certain threshold (currently 100 bytes) will be compressed during storage and decompressed during retrieval transparently. Default: True' )
