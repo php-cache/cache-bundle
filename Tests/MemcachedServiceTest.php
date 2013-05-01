215729981fa3ebbb60912593d9f49c4eb1f0f316
Aaron Scherer <aequasi@gmail.com>
2013-05-01 10:45:30 -0700

    Fixing namespaces

--- Tests/MemcachedServiceTest.php
+++ Tests/MemcachedServiceTest.php
@@ -4,8 +4,7 @@
  * @date 2013
  * @license   http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
  */
-
-namespace Doctrine\Bundle\DoctrineBundle\Tests;
+namespace Aequasi\Bundle\MemcachedBundle\Tests;
 
 /**
  * Class MemcachedServiceTest
