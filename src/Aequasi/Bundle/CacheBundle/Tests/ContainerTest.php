db0d4e89d56b741a8545ca93f739ad7b732dcb40
Aaron Scherer <aaron@undergroundelephant.com>
2013-12-11 17:01:16 -0800

    Changing autoloader location

--- src/Aequasi/Bundle/CacheBundle/Tests/ContainerTest.php
+++ src/Aequasi/Bundle/CacheBundle/Tests/ContainerTest.php
@@ -19,7 +19,7 @@ class ContainerTest extends TestCase
          */
         public function testContainer()
         {
-                $container = $this->createYamlBundleTestContainer();
+                $container = $this->createContainer();
 
                 // @TODO Create Tests.....
                 $this->assertTrue( true );
