5f9e11a3483fe6040382c5ccf853be8143e3ef67
Aaron Scherer <aaron@undergroundelephant.com>
2013-12-11 16:01:03 -0800

    Adding unit tests

---
+++ src/Aequasi/Bundle/CacheBundle/Tests/DependencyInjection/AequasiCacheExtensionTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @author    Aaron Scherer
+ * @date      12/11/13
+ * @copyright Underground Elephant
+ */
+
+namespace Aequasi\Bundle\CacheBundle\Tests\DependencyInjection;
+
+
+use Aequasi\Bundle\CacheBundle\Tests\TestCase;
+
+class AequasiCacheExtensionTest extends TestCase
+{
+	public function testServiceBuilder()
+	{
+		$container = $this->createContainerFromFile('service');
+
+
+		$config = $container->getParameter( $this->getAlias() . '.instances' );
+
+		foreach( array( 'memcached', 'redis' ) as $type ) {
+
+			$this->assertTrue( isset( $config[ $type ] ) );
+
+			$this->assertTrue( $container->hasDefinition( $this->getAlias() . '.instance.' . $type ) );
+			$this->assertTrue( $container->hasDefinition( $this->getAlias() . '.' . $type ) );
+
+			$this->assertInstanceOf( 'Aequasi\Bundle\CacheBundle\Service\CacheService', $container->get( $this->getAlias() . '.' . $type ) );
+			$this->assertInstanceOf( 'Doctrine\Common\Cache\Cache', $container->get( $this->getAlias() . '.' . $type )->getCache() );
+
+			$function = 'get' . ucwords( $type );
+			$this->assertInstanceOf( ucwords( $type ), $container->get( $this->getAlias() . '.' . $type )->getCache()->{$function}() );
+		}
+	}
+
+	public function testRouterBuilder()
+	{
+		$container = $this->createContainerFromFile( 'router' );
+
+		$config = $container->getParameter( $this->getAlias() . '.router' );
+
+		$this->assertTrue( isset( $config[ 'enabled' ] ) );
+		$this->assertTrue( isset( $config[ 'instance' ] ) );
+
+		$this->assertTrue( $config[ 'enabled' ] );
+		$this->assertEquals( $config[ 'instance' ], 'default' );
+
+		$this->assertInstanceOf( 'Aequasi\Bundle\CacheBundle\Routing\Router', $container->get( 'router' ) );
+	}
+
+	private function getAlias()
+	{
+		return 'aequasi_cache';
+	}
+} 
---
+++ src/Aequasi/Bundle/CacheBundle/Tests/TestCase.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @author    Aaron Scherer <aequasi@gmail.com>
+ * @date      2013
+ * @license   http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
+ */
+
+namespace Aequasi\Bundle\CacheBundle\Tests;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+
+use Aequasi\Bundle\CacheBundle\DependencyInjection\AequasiCacheExtension;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * Class TestCase
+ *
+ * @package Aequasi\Bundle\MemcachedBundle\Tests
+ */
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+
+	protected function loadFromFile( ContainerBuilder $container, $file )
+	{
+		$loader = new YamlFileLoader( $container, new FileLocator( __DIR__ . '/Fixtures' ) );
+		$loader->load( $file . '.yml' );
+	}
+
+	protected function createContainer( array $data = array() )
+	{
+		return new ContainerBuilder( new ParameterBag( array_merge( array(
+	    'kernel.bundles' => array( 'FrameworkBundle' => 'Symfony\\Bundle\\FrameworkBundle\\FrameworkBundle' ),
+	    'kernel.cache_dir' => __DIR__,
+	    'kernel.debug' => false,
+	    'kernel.environment' => 'test',
+	    'kernel.name' => 'kernel',
+	    'kernel.root_dir' => __DIR__,
+		), $data ) ) );
+	}
+
+	protected function createContainerFromFile( $file, $data = array() )
+	{
+		$container = $this->createContainer( $data );
+		$container->registerExtension( new AequasiCacheExtension() );
+		$this->loadFromFile( $container, $file );
+
+		$container->getCompilerPassConfig()
+		          ->setOptimizationPasses( array() );
+		$container->getCompilerPassConfig()
+		          ->setRemovingPasses( array() );
+		$container->compile();
+
+		return $container;
+	}
+}
