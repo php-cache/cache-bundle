6f97509a676acb34c7a1c2b7741559405af3c45d
Aaron Scherer <aaron@undergroundelephant.com>
2013-12-11 16:57:21 -0800

    Changing autoloader location

--- src/Aequasi/Bundle/CacheBundle/Tests/bootstrap.php
+++ src/Aequasi/Bundle/CacheBundle/Tests/bootstrap.php
@@ -5,7 +5,7 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0Elephant
  */
 
-if( !@include __DIR__ . '/../vendor/autoload.php' ) {
+if( !@include __DIR__ . '/../../../../../vendor/autoload.php' ) {
 	die( "You must set up the project dependencies, run the following commands:
 wget http://getcomposer.org/composer.phar
 php composer.phar install --dev
