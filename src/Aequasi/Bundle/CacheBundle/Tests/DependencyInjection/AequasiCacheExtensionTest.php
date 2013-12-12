10bd35e5feb1fbcefb68fafe9cc11804c3e85e09
Aaron Scherer <aaron@undergroundelephant.com>
2013-12-11 17:23:56 -0800

    Fixing config requirements

--- src/Aequasi/Bundle/CacheBundle/Tests/DependencyInjection/AequasiCacheExtensionTest.php
+++ src/Aequasi/Bundle/CacheBundle/Tests/DependencyInjection/AequasiCacheExtensionTest.php
@@ -26,7 +26,7 @@ class AequasiCacheExtensionTest extends TestCase
 			$this->assertTrue( $container->hasDefinition( $this->getAlias() . '.instance.' . $type ) );
 			$this->assertTrue( $container->hasDefinition( $this->getAlias() . '.' . $type ) );
 
-			$this->assertInstanceOf( 'Aequasi\Bundle\CacheBundle\Service\CacheService', $container->get( $this->getAlias() . '.' . $type ) );
+			$this->assertInstanceOf( 'Aequasi\Bundle\CacheBundle\Service\CacheService', $container->get( $this->getAlias() . '.instance.' . $type ) );
 			$this->assertInstanceOf( 'Doctrine\Common\Cache\Cache', $container->get( $this->getAlias() . '.' . $type )->getCache() );
 
 			$function = 'get' . ucwords( $type );
