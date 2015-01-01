bf3a259d118e19d9921ee94b1a9b123382f3f0b2
Julius Beckmann <github@h4cc.de>
2015-01-01 14:09:35 +0100

    Fixing test/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,9 +6,10 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
  */
 
-if (!@include __DIR__ . '/../../../../../vendor/autoload.php') {
-    die("You must set up the project dependencies, run the following commands:
+if (!@include __DIR__ . '/../../../../../vendor/autoload.php' || !@include __DIR__ . '/../vendor/autoload.php') {
+    echo "You must set up the project dependencies, run the following commands:
 wget http://getcomposer.org/composer.phar
 php composer.phar install --dev
-");
+";
+    exit(1);
 }
