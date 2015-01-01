1688ae4a2ce8b2405dd2f54a67db10a50712364c
Julius Beckmann <github@h4cc.de>
2015-01-01 14:28:47 +0100

    Using own phpunit #40 Fixed autoloading with tests #38

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,10 +6,19 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
  */
 
-if (!@include __DIR__ . '/../../../../../vendor/autoload.php' && !@include __DIR__ . '/../vendor/autoload.php') {
+$autoloadFile = false;
+foreach(array(__DIR__ . '/../../../../../vendor/autoload.php', __DIR__ . '/../vendor/autoload.php') as $file) {
+    if(is_file($file)) {
+        $autoloadFile = $file;
+    }
+}
+
+if (!$autoloadFile) {
     echo "You must set up the project dependencies, run the following commands:
 wget http://getcomposer.org/composer.phar
 php composer.phar install --dev
 ";
     exit(1);
 }
+
+require $autoloadFile;
