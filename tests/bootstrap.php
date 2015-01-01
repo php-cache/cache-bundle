d98a69b54d06d924a9d6529630af12fd64802fed
Julius Beckmann <github@h4cc.de>
2015-01-01 14:12:36 +0100

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
  */
 
-if (!@include __DIR__ . '/../../../../../vendor/autoload.php' || !@include __DIR__ . '/../vendor/autoload.php') {
+if (!@include __DIR__ . '/../../../../../vendor/autoload.php' && !@include __DIR__ . '/../vendor/autoload.php') {
     echo "You must set up the project dependencies, run the following commands:
 wget http://getcomposer.org/composer.phar
 php composer.phar install --dev
