bd2f4b6dafd7a24ac13fb90b39375b5999168f38
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-08-07 09:00:34 -0400

    Applied fixes from StyleCI

--- tests/Functional/BundleInitializationTest.php
+++ tests/Functional/BundleInitializationTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of php-cache\cache-bundle package.
+ *
+ * (c) 2015-2015 Aaron Scherer <aequasi@gmail.com>, Tobias Nyholm <tobias.nyholm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Cache\CacheBundle\Tests\Functional;
 
 class BundleInitializationTest extends BaseTestCase
