54a3bbde27b745ef2b3124d6e5b2785fc01a579c
Nyholm <tobias.nyholm@gmail.com>
2018-01-13 22:35:16 +0100

    cs

--- tests/Functional/DummyNormalizer.php
+++ tests/Functional/DummyNormalizer.php
@@ -1,12 +1,21 @@
 <?php
 
+/*
+ * This file is part of php-cache\cache-bundle package.
+ *
+ * (c) 2015 Aaron Scherer <aequasi@gmail.com>, Tobias Nyholm <tobias.nyholm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Cache\CacheBundle\Tests\Functional;
 
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class DummyNormalizer implements NormalizerInterface
 {
-    public function normalize($object, $format = null, array $context = array())
+    public function normalize($object, $format = null, array $context = [])
     {
         return [];
     }
